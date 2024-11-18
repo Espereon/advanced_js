d44cd66d4decf3eca95c386e31c2e2e3bceb0867
Espereon <announcement32@post.com>
2024-11-18 19:31:27 +0300

    le1_hw

--- lesson_1_hw/script.js
+++ lesson_1_hw/script.js
@@ -20,22 +20,42 @@ const musicCollection = [
         year: "2021"
     }
 ];
-// console.log(range);
-// let arr = Array.from(musicCollection);
-// console.log(arr);
-
 
 musicCollection[Symbol.iterator] = function () {
     return {
-        current: this.title,
-        last: this.year,
+        current: 0,
+        to: this.length,
         next() {
-            return this.current <= this.last ? { done: false, value: this.current++ } :
-                { done: true };
+            return this.current < this.to
+                ? { done: false, value: musicCollection[this.current++] }
+                : { done: true };
         }
-    };
+    }
 };
 
 for (let album of musicCollection) {
-    console.log(album);
-}
+    console.log(`Название: ${album.title}, автор: ${album.artist}, год издания: ${album.year}`);
+};
+
+const chefsMap = new Map([
+    ["Пицца 'Маргарита'", "Виктор"],
+    ["Пицца 'Пепперони'", "Виктор"],
+    ["Суши 'Филадельфия'", "Ольга"],
+    ["Суши 'Калифорния'", "Ольга"],
+    ["Тирамису", "Дмитрий"],
+    ["Чизкейк", "Дмитрий"],
+]);
+const ordersMap = new Map();
+
+const client1 = { name: "Алексей" };
+const client2 = { name: "Мария" };
+const client3 = { name: "Ирина" };
+
+ordersMap.set(client1, ["Пицца 'Пепперони'", "Тирамису"]);
+ordersMap.set(client2, ["Суши 'Калифорния'", "Пицца 'Маргарита'"]);
+ordersMap.set(client3, ["Чизкейк"]);
+console.log(ordersMap);
+
+ordersMap.forEach((order, client) => {
+    console.log(`Клиент ${client.name} заказал: ${order}`);
+});
