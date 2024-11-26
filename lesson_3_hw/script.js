52af180df277a3d7601f7693c5042acb6d501a8e
Espereon <announcement32@post.com>
2024-11-27 02:14:31 +0300

    l3_hw2

--- lesson_3_hw/script.js
+++ lesson_3_hw/script.js
@@ -2,42 +2,22 @@
 const productField = document.getElementById('product-name');
 const productReview = document.getElementById('product-review');
 const addButton = document.getElementById('add-button')
-const reviewLists = document.getElementById('review-list');
 
-// получаем сохраненный список покупок из LocalStorage (Если есть)
-let reviewList = JSON.parse(localStorage.getItem('reviewList')) || [];
 
-// функция для обновления списка покупок на странице и сохранения его в LocalStorage
-const updateReviewlist = () => {
-    //Очищаем список покупок на страница
-    reviewList.innerHTML = '';
-    //Создаём новые элементы списка для каждого пункта
-    reviewList.forEach((product, review) => {
-        const nameProduct = document.createElement('li');
-        const reviewProduct = document.createElement('p');
-        nameProduct.textContent = product;
-        reviewProduct.textContent = review;
-        reviewLists.append(nameProduct);
-        nameProduct.appendChild(reviewProduct);
+let data = JSON.parse(localStorage.getItem("data"));
+
+if (data === null) {
+    data = [];
+} else {
+    data = JSON.parse(localStorage.getItem("data"));
+}
+
+addButton.addEventListener("click", function () {
+    data.push({
+        product: `${productField.value}`,
+        reviews: `${productReview.value}`,
     });
-    // Сохраняем обновленный список покупок в localStorage
-    localStorage.setItem('reviewList', JSON.stringify(reviewList));
-};
-
-// Обработчик события для кнопки "добавить"
-addButton.addEventListener('click', () => {
-    const newProduct = productField.value.trim();
-    const newReview = productReview.value.trim();
-    if (newProduct !== '' || newReview !== '') {
-        //Добавляем новый пункт в список покупок
-        reviewList.push(newProduct, newReview);
-        productField.value = '';
-        productReview.value = ''
-        updateReviewlist();
-    }
+    localStorage.setItem(`data`, JSON.stringify(data, null, 2));
 });
-
-// Обновляем список покупок при загрузке страницы
-updateReviewlist();
 
 localStorage.clear();
